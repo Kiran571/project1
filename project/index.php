baa9c4aa3c2371d66b67d162166453f8b3debec7
Kiran <kiranvsuryawanshi05@gmail.com>
2020-07-19 21:09:44 -0700

    i

---
+++ project/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login and Registration Forms</title>
+	<link rel="stylesheet"  href="final-master/style.css">
+</head>
+<body>
+
+
+		<div class="hero">
+			<h1 class="hh"><br><br> RK Restaurant</h1>
+			<div class="form-box">
+				<div class="button-box">
+					<div id="btn"></div>
+					<button type="button" class="toggle-btn" onclick="login()">Log In</button>
+					<button type="button" class="toggle-btn" onclick="Register()">Register</button>
+				</div>
+				<form id="login" class="input-group">
+					<input type="text" class="input-field" placeholder="User name" name="uid" required="">
+					<input type="password" class="input-field" placeholder="password" name="pwd" required="">
+					<input type="checkbox" class="chech-box" ><span>Remember password</span>
+					<button type="submit" class="submit-btn">Login</button>
+				</form>
+
+				<form id="Register" class="input-group">
+					<input type="text" class="input-field" placeholder="User name" name="uid" required="">
+					<input type="text" class="input-field" placeholder="email" name="mail" required="">
+					<input type="password" class="input-field" placeholder="password" name="pwd" required="">
+					<input type="password" class="input-field" placeholder="Repeat-password" name="Repeat-pwd" required="">
+					<input type="checkbox" class="chech-box" ><span>I agree to the terms & conditions</span>
+					<button type="submit" class="submit-btn">Register</button>
+
+
+				</form>
+			</div>
+		</div>
+
+		<script>
+			var x=document.getElementById("login");
+			var y=document.getElementById("Register");
+			var z=document.getElementById("btn");
+
+
+			function Register() {
+
+				x.style.left="-400px";
+				y.style.left="50px";
+				z.style.left="110px";
+
+			}
+
+			function login() {
+
+				x.style.left="50px";
+				y.style.left="-400px";
+				z.style.left="0";
+
+			}
+
+		</script>
+
+
+
+
+
+</body>
+</html>
